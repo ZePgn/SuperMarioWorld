ba84476741f7f2e8138917800900e683667a95ae
Felipe <zepagono@gmail.com>
2024-09-15 10:54:57 -0300

    Versão 3
    Adicionado musica de inicio;
    Adicionado som de morte do personagem;
    Adicionado a ação de morrer antes do tubo;
    Adicionado placar para pontuação.

--- js/script.js
+++ js/script.js
@@ -1,38 +1,65 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
+const music = document.getElementById('marioMusic');
+const gameOverMusic = document.getElementById('gameOverMusic');
+const scoreElement = document.getElementById('score');
 
-const jump = () =>{
-    mario.classList.add('jump');
+let score = 0;
+let gameRunning = true;
 
-    setTimeout(() =>{
 
-        mario.classList.remove('jump');
-
-    }, 500);
+const playMusic = () => {
+    music.loop = true;
+    music.play();
 }
 
-const loop = setInterval(() =>{
+
+const jump = () => {
+    if (gameRunning) {
+        mario.classList.add('jump');
+
+        setTimeout(() => {
+            mario.classList.remove('jump');
+        }, 500);
+    }
+}
+
+
+const loop = setInterval(() => {
 
     console.log('loop')
 
     const pipePosition = pipe.offsetLeft;
-    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px','');
-    
-    if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 105) {
 
         pipe.style.animation = 'none';
-        pipe.style.animation = `${pipePosition}px`;
+        pipe.style.left = `${pipePosition}px`;
 
         mario.style.animation = 'none';
         mario.style.bottom = `${marioPosition}px`;
 
         mario.src = 'img/game-over.png';
-        mario.style.width = '75px';
-        mario.style.marginLeft = '80px'
+        mario.style.width = '75px'
+        mario.style.marginLeft = '50px'
 
         clearInterval(loop);
+
+        gameOverMusic.play();
+        music.pause();
+
     }
 
-} , 10);
+    if (gameRunning) {
+        score += 1; // Incrementa a pontuação
+        scoreElement.textContent = `Score: ${score}`; // Atualiza o elemento com a pontuação
+    }
+
+}, 100);
+
 
 document.addEventListener('keydown', jump);
+document.addEventListener('touchstart', jump);
+document.addEventListener('keydown', playMusic);  
+        
